3822ea6302d48eb7a78c01ccd114633ccaa60434
avivhl789 <avivhl1212@gmail.com>
2021-01-11 17:05:56 +0200

    shortpath

--- Ex3/src/GraphAlgo.py
+++ Ex3/src/GraphAlgo.py
@@ -3,6 +3,7 @@ from typing import List
 
 from GraphAlgoInterface import GraphAlgoInterface
 from DiGraph import DiGraph, Node
+from queue import PriorityQueue
 
 
 class GraphAlgo(GraphAlgoInterface):
@@ -46,7 +47,49 @@ class GraphAlgo(GraphAlgoInterface):
         return True
 
     def shortest_path(self, id1: int, id2: int) -> (float, list):
-        pass
+        if self.get_graph().v_size() < 2:
+            return -1, []
+        if self.get_graph().v_size() == 2:
+            if id2 in self.get_graph().all_out_edges_of_node(id1):
+                return self.get_graph().all_out_edges_of_node(id1)[id2], [id1, id2]
+
+        else:
+            if id1 in self.get_graph().dictionary and id2 in self.get_graph().dictionary:
+                pq = PriorityQueue()
+                cost = dict()
+                path = dict()
+                n = self.get_graph().dictionary.get(id1)
+                cost[id1] = 0
+                w = -1.0
+                pq.put(cost[id1], id1)
+                while not pq.empty():
+                    n = pq.get()
+                    for i in self.get_graph().all_out_edges_of_node(n):
+                        ni = i
+                        if not ni in cost:
+                            cost[ni] = cost.get(n.getKey) + self.get_graph().all_out_edges_of_node(n).get(ni)
+                            pq.put(cost[ni], ni)
+                            path[ni] = n
+                        elif cost[ni] > cost.get(n.getKey) + self.get_graph().all_out_edges_of_node(n).get(ni):
+                            cost[ni] = cost.get(n.getKey) + self.get_graph().all_out_edges_of_node(n).get(ni)
+                            pq.put(cost[ni], ni)
+                            path[ni] = n
+                        if ni == id2:
+                            if w == -1.0:
+                                w = cost[ni]
+                            else:
+                                w = min(w, cost[ni])
+                if w == -1.0:
+                    return -1, []
+                else:
+                    revpathlist = []
+                    x = id2
+                    while x == id1:
+                        revpathlist.__add__(path[x])
+                        x = path[x]
+                    revpathlist.__add__(id1)
+
+                    return w, revpathlist.reverse()
 
     def connected_component(self, id1: int) -> list:
         pass
@@ -76,7 +119,7 @@ class GraphAlgo(GraphAlgoInterface):
         edgeslist = []
         for i in self.get_graph().get_all_v():
             for j in self.get_graph().get_neighbors(i):
-                edgeslist.append({"src": int(j.get_src()), "dest": int(j.get_dest()), "w": float(j.get_weight())})
+                edgeslist.append({"src": j.get_src(), "dest": j.get_dest(), "w": j.get_weight()})
 
         graph_data = {"Edges": edgeslist, "Nodes": nodeslist}
 
